8c3ef7e4fdc33d65c3b481524bd7f75fe2391f0c
Ashish <ashishmaan22@gmail.com>
2024-09-11 15:57:18 +0530

    Aim Walking

--- Source/Blaster/BlasterComponents/CombatComponent.cpp
+++ Source/Blaster/BlasterComponents/CombatComponent.cpp
@@ -7,17 +7,34 @@
 
 UCombatComponent::UCombatComponent() {
 	PrimaryComponentTick.bCanEverTick = false;
+
+	BaseWalkSpeed = 600.0f;
+	AimWalkSpeed = 450.0f;
 }
 
 
 void UCombatComponent::BeginPlay() {
 	Super::BeginPlay();
+	if(Character) {
+		Character->GetCharacterMovement()->MaxWalkSpeed = BaseWalkSpeed;
+	}
 }
 
 void UCombatComponent::SetAiming(bool bIsAiming) {
 	bAiming = bIsAiming;
 
 	ServerSetAiming(bIsAiming);
+
+	if(Character) {
+		Character->GetCharacterMovement()->MaxWalkSpeed = bIsAiming ? AimWalkSpeed : BaseWalkSpeed;
+	}
+}
+
+void UCombatComponent::ServerSetAiming_Implementation(bool bIsAiming) {
+	bAiming = bIsAiming;
+	if(Character) {
+		Character->GetCharacterMovement()->MaxWalkSpeed = bIsAiming ? AimWalkSpeed : BaseWalkSpeed;
+	}
 }
 
 void UCombatComponent::OnRep_EquippedWeapon() {
@@ -25,10 +42,6 @@ void UCombatComponent::OnRep_EquippedWeapon() {
 		Character->GetCharacterMovement()->bOrientRotationToMovement = false;
 		Character->bUseControllerRotationYaw = true;
 	}
-}
-
-void UCombatComponent::ServerSetAiming_Implementation(bool bIsAiming) {
-	bAiming = bIsAiming;
 }
 
 void UCombatComponent::TickComponent(float DeltaTime, ELevelTick TickType,
