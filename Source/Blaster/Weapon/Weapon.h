ae3d63a0a8183a5b31cc91cf85f142a75c4b014b
Ashish <ashishmaan22@gmail.com>
2024-09-11 10:35:49 +0530

    weapon class

---
+++ Source/Blaster/Weapon/Weapon.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Weapon.generated.h"
+
+UENUM(BlueprintType)
+enum class EWeaponState : uint8 {
+	EWS_Initial UMETA(DisplayName = "Initial State"),
+	EWS_Equipped UMETA(DisplayName = "Equipped"),
+	EWS_Dropped UMETA(DisplayName = "Dropped"),
+
+	EWS_Max UMETA(DisplayName = "DefaultMAX")
+};
+
+
+class USphereComponent;
+class USkeletalMeshComponent;
+
+
+UCLASS()
+class BLASTER_API AWeapon : public AActor {
+	GENERATED_BODY()
+
+public:
+	AWeapon();
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+
+	UPROPERTY(VisibleAnywhere, Category = "Weapon Properties")
+	USkeletalMeshComponent* WeaponMesh;
+
+	UPROPERTY(VisibleAnywhere, Category = "Weapon Properties")
+	USphereComponent* AreaSphere;
+
+	UPROPERTY(VisibleAnywhere)
+	EWeaponState WeaponState;
+
+	
+};
