e3344a9defe99e22ec45a172b0557a2ac02bd93d
Ashish <ashishmaan22@gmail.com>
2024-09-12 16:32:57 +0530

    Drawing crosshairs

--- Source/Blaster/HUD/BlasterHUD.h
+++ Source/Blaster/HUD/BlasterHUD.h
@@ -33,6 +33,8 @@ private:
 
 	FHUDPackage HUDPackage;
 
+	void DrawCrosshairs(UTexture2D* Texture, FVector2D ViewportCenter);
+
 public:
 	FORCEINLINE void SetHUDPackage(const FHUDPackage& Package) { HUDPackage = Package; }
 	
